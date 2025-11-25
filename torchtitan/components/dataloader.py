4a614fcda2ae397d4856f041e76ae759db53b9dc
Corey Lowman <clowman@lambdal.com>
2025-11-25 16:50:27 +0000

    Dataloader num_workers=1, prefetch=2

--- torchtitan/components/dataloader.py
+++ torchtitan/components/dataloader.py
@@ -71,7 +71,7 @@ class ParallelAwareDataloader(StatefulDataLoader, BaseDataLoader):
         self.dp_world_size = dp_world_size
         self.dp_rank = dp_rank
         self.batch_size = batch_size
-        super().__init__(dataset, batch_size, collate_fn=collate_fn, pin_memory=True)
+        super().__init__(dataset, batch_size, collate_fn=collate_fn, pin_memory=True, num_workers=1, prefetch_factor=2)
         self._rank_id = f"dp_rank_{dp_rank}"
 
     def state_dict(self) -> dict[str, Any]:
