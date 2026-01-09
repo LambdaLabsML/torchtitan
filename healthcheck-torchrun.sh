df1674e32a3a78558060f1fb93db449b7a9d6e71
Corey Lowman <clowman@lambdal.com>
2026-01-09 18:38:23 +0000

    Removing unused files

--- healthcheck-torchrun.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-torchrun \
-    --master-addr ${HEAD_IP} \
-    --master-port ${HEAD_PORT} \
-    --node-rank ${SLURM_NODEID} \
-    --nnodes ${SLURM_NNODES} \
-    --nproc-per-node 4 \
-    healthcheck.py "$@"
--- slurm-torchrun.sh
+++ slurm-torchrun.sh
@@ -4,5 +4,5 @@ torchrun \
     --master-port ${HEAD_PORT} \
     --node-rank ${SLURM_NODEID} \
     --nnodes ${SLURM_NNODES} \
-    --nproc-per-node 4 \
+    --nproc-per-node ${GPUS_PER_NODE} \
     -m torchtitan.train "$@"
